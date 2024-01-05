48dd28561baebc8d3a644bf07cceb8ec519833f3
yatin.sarvaiya <yatin.sarvaiya@krishtechnolabs.com>
2024-01-05 17:45:13 +0530

    react practice

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Inner from './Components/BodyContent/Inner';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className="Myapp">
+      <Header />
+      <Inner />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
